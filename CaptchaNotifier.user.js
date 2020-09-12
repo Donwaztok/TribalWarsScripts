4b90b0d300b8778c4e3dcc2882e225172c6b1d5b
Donwaztok <igor.rmartins@outlook.com>
2020-09-11 21:45:05 -0300

    Modificado CaptchaClicker para CaptchaNotifier

--- CaptchaNotifier.user.js
+++ CaptchaNotifier.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name                Captcha Clicker
+// @name                Captcha Notifier
 // @version             1.0
 // @description         Clica automatimente no verificador Captcha
 // @author              Igor Martins
@@ -13,9 +13,16 @@
 
 (function () {
     'use strict';
+    Notification.requestPermission();
     setTimeout(function () {
         if (document.getElementsByClassName("recaptcha-checkbox-checkmark") !== undefined && document.getElementsByClassName("recaptcha-checkbox-checkmark")[0] !== undefined) {
             document.getElementsByClassName("recaptcha-checkbox-checkmark")[0].click();
+            var n = new Notification('Porfavor resvola o Captcha');
+            document.addEventListener('visibilitychange', function () {
+                if (document.visibilityState === 'visible') {
+                    n.close();
+                }
+            });
         }
     }, 800);
 })();
